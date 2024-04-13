b3fb960b7333623b6f7d6490427230a1bf04ecfc
Henrique Amaral <henriqueamaraldev@gmail.com>
2024-04-13 12:50:55 -0300

    feat: Passando a aplicação para SSR e mantendo uma pagina estatica funcionando atraves da constante prerender definida no arquivo [slug].astro

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -2,7 +2,15 @@
 export default {
 	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
 	theme: {
-		extend: {},
+		extend: {
+			colors: {
+				primary: '#030520',
+				secondary: '#0616EF',
+				tertiary: '#CBD5E1',
+				dark: '#252A34',
+				light: '#EAEAEA'
+			}
+		}
 	},
 	plugins: [require("@tailwindcss/typography")],
 }
